75b82ebcc0560c248c3274f347856ee92a7351c9
Moisès Coll Macià <moisescollmacia@gmail.com>
2021-11-14 13:17:26 +0100

    Update raju.sh

--- racimolab_jupiternotebook/raju.sh
+++ racimolab_jupiternotebook/raju.sh
@@ -1,67 +1,129 @@
-#!/bin/sh
+#!/bin/bash
 
-# command to run this script:
-# $ bash raju.sh [servernumber] [kill]
-# - servernumber: it is a two digit number from 1 to 7 (01, 02, ..., 07). If you don't set this number, it will be set automatically as the variable "def_c" 
-# - kill: it will kill all tmux sessions started by this script 
+#1. Defining default variables
+u="XXXXXX"             #Set your KU USER in this variable
+e="ENVENV"             #Set your conda enviroment in this variable
+c=1111                 #Set your port id for the jupyter notebook here (be creative)
+l=2222                 #Set your port id for the ssh tunel here (be creative)
+m="jupyter_notebook_c" #This will be the name of the tmux session started on the RacimoLab server. You can change it if you want or leave it as is.
+n="jupyter_notebook_l" #This will be the name of the tmux session started on your local machine. You can change it if you want or leave it as is.
+s="01"                 #Default server number. You can change it to be which ever default to want to set
+k=""                   #Do not modify this variable
+r=""                   #Do not modify this variable
+             
+#2. help function
 
+help()
+{
+    echo "Usage: bash $0 [-u,-e,-c,-l,-m,-n,-s,-k,-r,-h]
 
-#A. Defining variables
-ku_user="XXXXXX"        #Set your KU USER in this variable
-envname="ENVENV"        #Set your conda enviroment in this variable
-cp=9999                 #Set your port id for the jupyter notebook here (be creative)
-lp=7777                 #Set your port id for the ssh tunel here (be creative)
-m1="jupyter_notebook_c" #This will be the name of the tmux session started on the RacimoLab server. You can change it if you want or leave it as is.
-m2="jupyter_notebook_l" #This will be the name of the tmux session started on your local machine. You can change it if you want or leave it as is.
-c_def="04"              #Default server number. You can change it to be which ever default to want to set
+  Command                     Explanation                         Default                
+  -------                     -----------                         -------                 
+  -u, --user <string>         KU user                             ${u}
+  -e, --env <string>          conda environment                   ${e}
+  -c, --clusterport <int>     cluster port                        ${c}
+  -l, --localport <int>       local port                          ${l}
+  -m, --clustertmux <string>  cluster tmux session name           ${m}
+  -n, --localtmux <string>    local tmux session name             ${n}
+  -s, --serverid <01-07>      RacimoServer ID                     ${s}
+  -k, --kill                  Kill the tmux sessions which will    
+                              stop the ssh tunnels and jupyter      
+                              notebook
+  -r, --reconnect             reestablishes the local tunnel to 
+                              reconnect with the cluster
+  -h, --help                  prints this help message
+  "
+    exit 1
+}
 
+#3. parssing variables
+while getopts "u:e:c:l:m:n:s:krh:" o; do
+    case "${o}" in
+        u)
+            u=${OPTARG}
+            ;;
+        e)
+            e=${OPTARG}
+            ;;
+        c)
+            c=${OPTARG}
+            ;;
+        l)
+            l=${OPTARG}
+            ;;
+        m)
+            m=${OPTARG}
+            ;;
+        n)
+            n=${OPTARG}
+            ;;
+        s)
+            [[ $s == "01" || $s == "02" || $s == "03" || $s == "04" || $s == "05" || $s == "06" || $s == "07" ]] || help
+            s=${OPTARG}
+            ;;
+        k)
+            k="kill"
+            ;;
+        r)
+            r="reconnect"
+            ;;
+        h)
+            help
+            ;;                      
+        *)
+            help
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-c=$1
-k=$2
-
-#A.1 
-if [ "${c}" != "01" ] && [ "${c}" != "02" ] && [ "${c}" != "03" ] && [ "${c}" != "04" ] && [ "${c}" != "05" ] && [ "${c}" != "06" ] && [ "${c}" != "07" ]
+#4. SCRIPT
+#A. If killing option enabled, kill the tmux sessions
+if [[ ${k} = "kill" ]]
 then
-	c="${c_def}"
-fi
-if [ "$1" == "kill" ]
+    echo "Killing tmux server running in racimocomp${s}fl"
+    ssh ${u}@racimocomp${s}fl 'tmux kill-session -t '${m}''
+    tmux kill-session -t ${n}
+    exit 0
+#B. If reconnect option enabled
+elif [[ ${r} = "reconnect" ]]
 then
-	c="${c_def}" 
-	k="kill"
-fi
-
-if [ "${k}" != "kill" ]
-then
-	#B. Step by  step
-	#B.1 Step 1 - Running jupyter notebook on a computing node 
-	if [ "`ssh ${ku_user}@racimocomp${c}fl 'tmux ls | egrep '${m1}''`" = "" ]
-	then
-		echo "1) Starting jupyter notebook at racimocomp${c}fl in a tmux session named ${m1} with port ${cp}"
-		ssh ${ku_user}@racimocomp${c}fl 'tmux new-session -s '${m1}' -d'
-		ssh ${ku_user}@racimocomp${c}fl 'tmux send-keys -t '${m1}' "conda activate '${envname}'" C-m "jupyter-lab --no-browser --port='${cp}'" C-m'
-	else
-		echo "1) racimocomp${c}fl already has a tmux session named ${m1}"
-	fi
-
-	#B.2 Step 2 - Open a ssh tunnel from your local machine to the RacimoLab server 
-	if [ "`tmux ls | egrep ${m2}`" = "" ]
-	then
-		echo "2) Starting a ssh tunnel from Local computer (port ${lp}) to RacimoLab server racimocomp${c}fl in a tmux session named ${m2}"
-		tmux new-session -s ${m2} -d
-		tmux send-keys -t ${m2}  "ssh ${ku_user}@racimocomp${c}fl -L '${lp}':localhost:'${cp}' -N" C-m
-
-		                                                                                                                                            
-	else
-		echo "2) Local computer already has a tmux session named ${m2}"
-	fi
-
-	#B.3 Step 3 - Jupyter notebook in your local machine browser
-	echo "3) Opening Google Chrome to access jupyter notebook on your local computer"
-	open --new -a "Google Chrome" --args "http://localhost:${lp}/"
+    #B.1 Create again the tmux session in case it does not exist and restart the ssh tunnel
+    if [ "`tmux ls | egrep ${n}`" = "" ]
+    then
+        echo "1) Retarting a ssh tunnel from Local computer (port ${l}) to RacimoLab server racimocomp${s}fl in a newly created tmux session named ${n}"
+        tmux new-session -s ${n} -d
+        tmux send-keys -t ${n}  "ssh ${u}@racimocomp${s}fl -L '${l}':localhost:'${c}' -N" C-m
+    #B.2 Restart the ssh tunnel
+    else
+        echo "1) Restarting a ssh tunnel from Local computer (port ${l}) to RacimoLab server racimocomp${s}fl in an existing tmux session named ${n}"
+        tmux send-keys -t ${n}  "ssh ${u}@racimocomp${s}fl -L '${l}':localhost:'${c}' -N" C-m
+    fi
+    exit 0
+#Automate all the GitHub steps
 else
-	#C. If killing option, kill the tmux sessions
-	echo "Killing tmux server running in racimocomp${c}fl"
-	ssh ${ku_user}@racimocomp${c}fl 'tmux kill-session -t '${m1}'' 
-	echo "Killing tmux server from local computer"
-	tmux kill-session -t ${m2}
+    ##C.1 Step 1 - Running jupyter notebook on a computing node 
+    if [ "`ssh ${u}@racimocomp${s}fl 'tmux ls | egrep '${m}''`" = "" ]
+    then
+        echo "1) Starting jupyter notebook at racimocomp${s}fl in a tmux session named ${m} with port ${c}"
+        ssh ${u}@racimocomp${s}fl 'tmux new-session -s '${m}' -d'
+        ssh ${u}@racimocomp${s}fl 'tmux send-keys -t '${m}' "conda activate '${e}'" C-m "jupyter-lab --no-browser --port='${c}'" C-m'
+    else
+        echo "1) racimocomp${s}fl already has a tmux session named ${m}"
+    fi
+
+
+    #C.2 Step 2 - Open a ssh tunnel from your local machine to the RacimoLab server 
+    if [ "`tmux ls | egrep ${n}`" = "" ]
+    then
+        echo "2) Starting a ssh tunnel from Local computer (port ${l}) to RacimoLab server racimocomp${s}fl in a tmux session named ${n}"
+        tmux new-session -s ${n} -d
+        tmux send-keys -t ${n}  "ssh ${u}@racimocomp${s}fl -L '${l}':localhost:'${c}' -N" C-m
+    else
+        echo "2) Local computer already has a tmux session named ${n}"
+    fi
 fi
+
+#D. Step 3 - Jupyter notebook in your local machine browser
+echo "3) Opening Google Chrome to access jupyter notebook on your local computer"
+open --new -a "Google Chrome" --args "http://localhost:${l}/"
